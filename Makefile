f6eae4a99928f08f731b130a6c9029ebd2a04915
Manjunath Patil <manjunath.b.patil@oracle.com>
2020-05-10 18:47:14 -0700

    oled: bring in oled cmd to oled-tools
    Signed-off-by: Manjunath Patil <manjunath.b.patil@oracle.com>

---
+++ Makefile
@@ -0,0 +1,58 @@
+# Copyright (c) 2020, Oracle and/or its affiliates.
+# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+#
+# This code is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 only, as
+# published by the Free Software Foundation.
+#
+# This code is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# version 2 for more details (a copy is included in the LICENSE file that
+# accompanied this code).
+#
+# You should have received a copy of the GNU General Public License version
+# 2 along with this work; if not, see <https://www.gnu.org/licenses/>.
+#
+# Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+# or visit www.oracle.com if you need additional information or have any
+# questions.
+
+TOPDIR=$$PWD
+subdirs=
+
+rev_subdirs:=$(shell echo -n "$(subdirs) " | tac -s ' ')
+BINDIR_PREFIX=/usr
+BINDIR=$(BINDIR_PREFIX)/sbin
+
+# run ./configure generate oled-env.sh
+-include oled-env.sh
+export OLED_DIST
+export PYTHON_SITEDIR
+
+all:
+	$(foreach dir,$(subdirs), make TOPDIR=$(TOPDIR) BINDIR=$(BINDIR)/oled-tools -C $(dir) all || exit 1;)
+
+clean:
+
+	$(foreach dir,$(subdirs), make TOPDIR=$(TOPDIR) BINDIR=$(BINDIR)/oled-tools -C $(dir) clean;)
+	[ -f oled-env.sh ] && rm -f oled-env.sh || :
+
+install:
+	@echo "install:$(CURDIR)"
+	make all
+	mkdir -p /etc/oled
+	mkdir -p $(BINDIR)/oled-tools
+	install -m 755 oled.py $(BINDIR)/oled
+	gzip -c oled.man > /usr/share/man/man8/oled.8.gz; chmod 644 /usr/share/man/man8/oled.8.gz
+	$(foreach dir,$(subdirs), make TOPDIR=$(TOPDIR) BINDIR=$(BINDIR)/oled-tools -C $(dir) install || exit 1;)
+	@echo "OLED_TOOLS successfully installed!"
+
+uninstall:
+	@echo "uninstall:$(CURDIR)"
+	$(foreach dir, $(rev_subdirs), make TOPDIR=$(TOPDIR) BINDIR=$(BINDIR)/oled-tools -C $(dir) uninstall || exit 1;)
+	rm -f /usr/share/man/man8/oled.8.gz
+	rm -f $(BINDIR)/oled
+	rmdir $(BINDIR)/oled-tools
+	rmdir /etc/oled
+	@echo "OLED_TOOLS successfully uninstalled!"
---
+++ oled.py
@@ -0,0 +1,135 @@
+#!/usr/bin/python
+#
+# Copyright (c) 2020, Oracle and/or its affiliates.
+# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+#
+# This code is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 only, as
+# published by the Free Software Foundation.
+#
+# This code is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# version 2 for more details (a copy is included in the LICENSE file that
+# accompanied this code).
+#
+# You should have received a copy of the GNU General Public License version
+# 2 along with this work; if not, see <https://www.gnu.org/licenses/>.
+#
+# Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+# or visit www.oracle.com if you need additional information or have any
+# questions.
+
+import sys
+import os
+import getpass
+import platform
+
+# Oracle Linux Enhanced Diagnostic Tools
+MAJOR = "0"
+MINOR = "1"
+RELEASE = "LATEST_UNSTABLE"
+
+BINDIR="/sbin/oled-tools"
+if not os.path.exists(BINDIR):
+	BINDIR="/usr/sbin/oled-tools"
+
+# cmds
+GATHER = BINDIR + "/gather"
+LKCE = BINDIR + "/lkce"
+SMTOOL = BINDIR + "/smtool"
+OLED_KDUMP = BINDIR + "/kdump"
+
+def help(error):
+    print("Oracle Linux Enhanced Diagnostic Tools")
+    print("Usage:")
+    print("  %s <command> <subcommand>" % sys.argv[0])
+    print("Valid commands:")
+    print("     smtool          -- Spectre-Meltdown tool")
+    print("     gather          -- system data gather")
+    print("     lkce            -- Linux Kernel Core Extractor")
+    print("     kdump           -- configure oled related kdump options")
+    print("     help            -- show this help message")
+    print("     version         -- print oled version")
+
+    if (error):
+        sys.exit(1)
+    sys.exit(0)
+
+def run_as_root():
+    if (getpass.getuser()) != "root":
+        print("Run as root only")
+        sys.exit(1)
+
+def cmd_version():
+	version = "%s.%s-%s"%(MAJOR, MINOR, RELEASE)
+	print(version)
+
+def cmd_smtool(args):
+    cmdline = SMTOOL
+    for arg in args:
+        cmdline = cmdline + " %s" % arg
+    ret = os.system(cmdline)
+    return ret
+
+def cmd_gather(args):
+    cmdline = GATHER
+    for arg in args:
+        cmdline = cmdline + " %s" % arg
+    ret = os.system(cmdline)
+    return ret
+
+def cmd_lkce(args):
+    cmdline = LKCE
+    for arg in args:
+        cmdline = cmdline + " %s" % arg
+    ret = os.system(cmdline)
+    return ret
+
+def cmd_kdump(args):
+    cmdline = OLED_KDUMP
+    for arg in args:
+        cmdline = cmdline + " %s" % arg
+    ret = os.system(cmdline)
+    return ret
+
+def cmd_help(args):
+    help(False)
+
+def main():
+    run_as_root()
+
+    if len(sys.argv) < 2:
+        help(True)
+
+    cmd = sys.argv[1]
+    args = sys.argv[2:]
+
+    if cmd == "smtool":
+        ret = cmd_smtool(args)
+        sys.exit(ret)
+
+    if cmd == "gather":
+        ret = cmd_gather(args)
+        sys.exit(ret)
+
+    if cmd == "lkce":
+        ret = cmd_lkce(args)
+        sys.exit(ret)
+
+    if cmd == "kdump":
+        ret = cmd_kdump(args)
+        sys.exit(ret)
+
+    if cmd == "version" or cmd == "--version":
+        ret = cmd_version()
+        sys.exit(ret)
+
+    if cmd == "help":
+        ret = cmd_help(args)
+        sys.exit(ret)
+
+    help(True)
+
+if __name__ == "__main__":
+    main()
