ryan-mccabe/oled-tools

commit 23032d5f108956d93cf260c576bf8d34bf654d64

Smtool: Add support for detection/mitigation of SRBDS
Add support for detection and mitigation of SRBDS
vulnerability.

Orabug: 33032240
Signed-off-by: Mridula Shastry <mridula.c.shastry@oracle.com>
Reviewed-by: Aruna Ramakrishna <aruna.ramakrishna@oracle.com>

buildrpm/oled-tools.spec
@@ -174,6 +174,9 @@ rm -rf $RPM_BUILD_ROOT
 %{_mandir}/man8/oled-topstack.8.gz
 
 %changelog
+* Thu Jun 24 2021 Mridula Shastry <mridula.c.shastry@oracle.com>
+- Smtool: Add support for detection/mitigation for SRBDS [Orabug: 33032240]
+
 * Mon Jun 14 2021 Mridula Shastry <mridula.c.shastry@oracle.com>
 - Smtool: Minor bug fixes [Orabug: 32999593]
 - Smtool: Fix scan/verbose scan and enable/disable mitigations

smtool/smtool_lib/boot.py
@@ -228,6 +228,11 @@ class Boot(Base):
             if os.path.exists(self.sysfile.get_sysfile()):
                 return ["tsx", "tsx_async_abort"]
 
+        if self.vtype == self.SRBDS:
+            self.sysfile = Sysfile(self.vtype)
+            if self.sysfile.get_sysfile() != None:
+                return ["srbds"]
+
         return []
 
     def add_boot_option(self, btype, vtype, opt, val):
@@ -452,6 +457,13 @@ class Boot(Base):
                     self.add_boot_option(btype, vtype, opt, self.ON)
                     return
 
+            if vtype == self.SRBDS:
+                if opt == "srbds":
+                    if targ == opt + "=off":
+                        self.boot_off = True
+                        self.add_boot_option(btype, vtype, opt, self.OFF)
+                        return
+
             if vtype == self.TSX_ASYNC_ABORT:
                 if opt == "tsx_async_abort":
                     if targ == opt + "=off":
@@ -469,6 +481,7 @@ class Boot(Base):
                     elif targ == opt + "=off":
                         self.boot_off = True
                         self.add_boot_option(btype, vtype, opt, self.ON)
+
         return
 
     def get_grub_info_xen(self):
@@ -674,6 +687,9 @@ class Boot(Base):
                 continue
             if arr[3] == self.OFF:
                 if variant.mitigated_kernel:
+                    if (arr[2] == "srbds"):
+                        remove = remove + arr[2] + "=off"
+                        continue
                     if (arr[1] == 2) or (arr[1] == 3):
                         add = add + arr[2] + "=on "
                     elif arr[1] == 4:
@@ -818,7 +834,8 @@ class Boot(Base):
                           self.L1TF,
                           self.MDS,
                           self.ITLB_MULTIHIT,
-                          self.TSX_ASYNC_ABORT]):
+                          self.TSX_ASYNC_ABORT,
+                          self.SRBDS]):
             raise ValueError("ERROR: Unrecognized boot variant")
 
         self.kver = kver

smtool/smtool_lib/cpu.py
@@ -86,7 +86,7 @@ class Cpu(Base):
 
         return self.vendor
 
-    v_1 = v_2 = v_3 = v_4 = v_5 = v_6 = v_7 = v_8 = None
+    v_1 = v_2 = v_3 = v_4 = v_5 = v_6 = v_7 = v_8 = v_9 = None
 
     def is_valid_cpu_vendor(self, vendor):
         """
@@ -413,11 +413,11 @@ class Cpu(Base):
             return
 
         self.v_1 = self.v_2 = self.v_3 = self.v_4 = True
-        self.v_5 = self.v_6 = self.v_7 = self.v_8 = True
+        self.v_5 = self.v_6 = self.v_7 = self.v_8 = self.v_9 = True
         if self.is_intel():
             if self.family == 5:
                 self.v_1 = self.v_2 = self.v_3 = self.v_4 = self.v_5 = False
-                self.v_7 = self.v_8 = False
+                self.v_7 = self.v_8 = self.v_9 = False
                 return
 
             if self.family == 6:
@@ -462,6 +462,18 @@ class Cpu(Base):
                                        self.INTEL_FAM6_SKYLAKE_X,
                                        self.INTEL_FAM6_KABYLAKE_DESKTOP]):
                     self.v_8 = False
+
+                if (self.model not in [self.INTEL_FAM6_IVYBRIDGE,
+                                       self.INTEL_FAM6_HASWELL_CORE,
+                                       self.INTEL_FAM6_HASWELL_ULT,
+                                       self.INTEL_FAM6_HASWELL_GT3E,
+                                       self.INTEL_FAM6_BROADWELL_GT3E,
+                                       self.INTEL_FAM6_BROADWELL_CORE,
+                                       self.INTEL_FAM6_SKYLAKE_MOBILE,
+                                       self.INTEL_FAM6_SKYLAKE_DESKTOP,
+                                       self.INTEL_FAM6_KABYLAKE_MOBILE,
+                                       self.INTEL_FAM6_KABYLAKE_DESKTOP]):
+                    self.v_9 = False
 
                 return
             return
@@ -578,6 +590,17 @@ class Cpu(Base):
         """
         return self.v_8
 
+    def is_vulnerable_v_9(self):
+        """
+        Checks if CPU is vulnerable to SRBDS.
+
+        Returns:
+        bool: True if it's vulnerable to SRBDS,
+        else return False.
+
+        """
+        return self.v_9
+
     def is_cpu_vulnerable(self):
         """
         Checks if CPU is vulnerable to any of the variants.
@@ -590,7 +613,8 @@ class Cpu(Base):
         if (self.is_vulnerable_v_1() or self.is_vulnerable_v_2() or
                 self.is_vulnerable_v_3() or self.is_vulnerable_v_4() or
                 self.is_vulnerable_v_5() or self.is_vulnerable_v_6() or
-                self.is_vulnerable_v_7() or self.is_vulnerable_v_8()):
+                self.is_vulnerable_v_7() or self.is_vulnerable_v_8() or
+                self.is_vulnerable_v_9()):
             return True
         return False
 
@@ -637,6 +661,9 @@ class Cpu(Base):
         if vtype == self.TSX_ASYNC_ABORT:
             return self.v_8
 
+        if vtype == self.SRBDS:
+            return self.v_9
+
     def display(self):
         """
         Displays cpu information and lists the variants
@@ -659,9 +686,10 @@ class Cpu(Base):
         print("     MDS        : " + str(self.is_vulnerable_v_6()))
         print("     ITLB_MULTIHIT: " + str(self.is_vulnerable_v_7()))
         print("    TSX_ASYNC_ABORT: " + str(self.is_vulnerable_v_8()))
+        print("    SRBDS: " + str(self.is_vulnerable_v_9()))
 
-    # Some cpus are not vulnerable to v_1,v_2,v_3,v_4,v_5,v_6,v_7 and
-    # v_8 variants.
+    # Some cpus are not vulnerable to v_1,v_2,v_3,v_4,v_5,v_6,v_7,v_8 
+    # and v_9 variants.
     def scan_vulnerabilities(self):
         self.check_all_cpus()
         return

smtool/smtool_lib/sysfile.py
@@ -78,6 +78,8 @@ class Sysfile(Base):
         "/sys/devices/system/cpu/vulnerabilities/itlb_multihit"
     SYS_TSX_ASYNC_ABORT_FILE = \
         "/sys/devices/system/cpu/vulnerabilities/tsx_async_abort"
+    SYS_SRBDS_FILE = \
+        "/sys/devices/system/cpu/vulnerabilities/srbds"
 
     # SMT Status file
     SYS_SMT_ACTIVE_FILE = "/sys/devices/system/cpu/smt/active"
@@ -200,6 +202,14 @@ class Sysfile(Base):
             self.sfile = self.SYS_ITLB_MULTIHIT_FILE
         if self.vtype == self.TSX_ASYNC_ABORT:
             self.sfile = self.SYS_TSX_ASYNC_ABORT_FILE
+        if self.vtype == self.SRBDS:
+            self.sfile = self.SYS_SRBDS_FILE
+
+        if (os.path.exists(self.sfile)):
+            return self.sfile
+        else:
+            return None
+
         return self.sfile
 
     def is_option_valid(self, index, option, kernel_ver):
@@ -384,7 +394,8 @@ class Sysfile(Base):
                           self.L1TF,
                           self.MDS,
                           self.ITLB_MULTIHIT,
-                          self.TSX_ASYNC_ABORT]):
+                          self.TSX_ASYNC_ABORT,
+                          self.SRBDS]):
             raise ValueError("ERROR: Unrecognized variant")
 
         self.vtype = vtype

smtool/smtool.py
@@ -110,7 +110,8 @@ class Smtool(Parser):
             return self.ITLB_MULTIHIT
         if vname == "TSX_Async_Abort":
             return self.TSX_ASYNC_ABORT
-
+        if vname == "SRBDS":
+            return self.SRBDS
 
     def is_enabled_runtime(self, vtype, vals):
         if (vtype == self.SPECTRE_V2):
@@ -354,7 +355,7 @@ class Smtool(Parser):
                   "" + disabled_variants_kernel[:-2])
             if (kernel.ktype == 1) or (kernel.ktype == 2):
                 print("Kernels older than UEK4 do not support mitigation "
-                      "for SSBD and ITLB_Multihit")
+                      "for SSBD, ITLB_Multihit and SRBDS")
                 print("Please upgrade the kernel to UEK4 version "
                       "4.1.12-124.33.2 to enable support for all "
                       "mitigations")

smtool/smtool_lib/base.py
@@ -62,6 +62,7 @@ class Base(object):
     MDS = 6
     ITLB_MULTIHIT = 7
     TSX_ASYNC_ABORT = 8
+    SRBDS = 9
     vdesc = [
         "Unknown",
         "Spectre V1",
@@ -71,7 +72,8 @@ class Base(object):
         "L1TF",
         "MDS",
         "ITLB_Multihit",
-        "TSX_Async_Abort"]
+        "TSX_Async_Abort",
+        "SRBDS"]
 
     # runtime settings
     IBRS_ENABLED = 1

smtool/smtool_lib/microcode.py
@@ -193,6 +193,27 @@ class Microcode(Base):
                      "85_4": ["0x2000064"],
                      "85_7": ["0x500002b"]}
 
+    SRBDS_MICROCODE = {"60_3": ["0x28"],
+                       "61_4": ["0x2f"],
+                       "69_1": ["0x26"],
+                       "70_1": ["0x1c"],
+                       "71_1": ["0x22"],
+                       "78_3": ["0xdc"],
+                       "85_3": ["0x1000157"],
+                       "85_4": ["0x2006906"],
+                       "85_6": ["0x4002f01"],
+                       "85_7": ["0x5002f01"],
+                       "94_3": ["0xdc"],
+                       "142_9": ["0xd6"],
+                       "142_10": ["0xd6"],
+                       "142_11": ["0xd6"],
+                       "142_12": ["0xd6"],
+                       "158_9": ["0xd6"],
+                       "158_10": ["0xd6"],
+                       "158_11": ["0xd6"],
+                       "158_12": ["0xd6"],
+                       "158_13": ["0xd6"]}
+
     def get_cur_microcode(self):
         """
         Gets current microcode version.
@@ -272,6 +293,14 @@ class Microcode(Base):
             if model_stepping_string in list(self.TAA_MICROCODE.keys()):
                 min_microcode_ver = int(
                     self.TAA_MICROCODE[model_stepping_string][0].
+                    split("0x")[1], 16)
+                return min_microcode_ver
+            return False
+
+        if vtype == self.SRBDS:
+            if model_stepping_string in list(self.SRBDS.keys()):
+                min_microcode_ver = int(
+                    self.SRBDS[model_stepping_string][0].
                     split("0x")[1], 16)
                 return min_microcode_ver
             return False

smtool/smtool_lib/parser.py
@@ -110,7 +110,8 @@ class Parser(Base):
         print("    SSBD,")
         print("    L1TF,")
         print("    ITLB_Multihit,")
-        print("    TSX Async Abort.")
+        print("    TSX Async Abort,")
+        print("    SRBDS.")
         print("NOTE:")
         print("    This script must be run as root and requires virt-what")
         print("    package to be installed on the machine.")

commit 6a54105cb7cfb92e4923a078ae3ade347c93c545

Smtool: Avoid failure when mitigation for a particular variant is not available
Smtool operations like scan, enabling/disabling mitigation fails when
mitigation for a particular variant (validated by the presence of status
sysfile which is dependent on the kernel version) is not present
on a system:

[root@ol6-uek4 oled-tools]# oled smtool --enable-default-mitigations -y
Traceback (most recent call last):
  File "/usr/lib/oled-tools/smtool", line 488, in <module>
    SMT = Smtool(sys.argv)
  File "/usr/lib/oled-tools/smtool", line 465, in __init__
    self.host.scan_host()
  File "/usr/lib/python2.6/site-packages/smtool_lib/host.py", line 189, in
scan_host
    self.vuln.scan_vulnerabilities()
  File "/usr/lib/python2.6/site-packages/smtool_lib/vulnerabilities.py", line
760, in scan_vulnerabilities
    is_mitigation_possible = self.v_7.scan_variant()
  File "/usr/lib/python2.6/site-packages/smtool_lib/variant.py", line 649, in
scan_variant
    if os.path.exists(self.sysfile.get_sysfile()):
  File "/usr/lib64/python2.6/genericpath.py", line 18, in exists
    st = os.stat(path)
TypeError: coercing to Unicode: need string or buffer, NoneType found

The issue was because of the issue introduced by commit
23032d5f1089 ('Smtool: Add support for detection/mitigation of SRBDS')
which modified return statements in get_sysfile function. Fix get_sysfile
function and also add appropriate checks in functions in other modules
to to avoid the above stacktrace.

Orabug: 33287128
Signed-off-by: Mridula Shastry <mridula.c.shastry@oracle.com>
Reviewed-by: Aruna Ramakrishna <aruna.ramakrishna@oracle.com>
Reviewed-by: Laurence Rochfort <laurence.rochfort@oracle.com>

buildrpm/oled-tools.spec
@@ -174,6 +174,10 @@ rm -rf $RPM_BUILD_ROOT
 %{_mandir}/man8/oled-topstack.8.gz
 
 %changelog
+* Thu Sep 02 2021 Mridula Shastry <mridula.c.shastry@oracle.com>
+- Smtool: Avoid failure when mitigation for a particular variant is
+ not available [Orabug: 33287128]
+ 
 * Wed Jul 07 2021 Manjunath Patil <manjunath.b.patil@oracle.com>
 - Prod release oled-tools-0.5-1
 

smtool/smtool_lib/boot.py
@@ -230,7 +230,7 @@ class Boot(Base):
 
         if self.vtype == self.SRBDS:
             self.sysfile = Sysfile(self.vtype)
-            if self.sysfile.get_sysfile() != None:
+            if os.path.exists(self.sysfile.get_sysfile()):
                 return ["srbds"]
 
         return []

smtool/smtool_lib/cpu.py
@@ -631,11 +631,10 @@ class Cpu(Base):
 
         """
         self.sysfile = Sysfile(vtype)
-        if(self.sysfile.get_sysfile() != None):
-            if (os.path.exists(self.sysfile.get_sysfile())):
-                if (self.sysfile.read_file(self.sysfile.get_sysfile())
-                    == "Not affected"):
-                    return False
+        if os.path.exists(self.sysfile.get_sysfile()):
+            if (self.sysfile.read_file(self.sysfile.get_sysfile())
+                == "Not affected"):
+                return False
 
         if vtype == self.SPECTRE_V1:
             return self.v_1

smtool/smtool_lib/sysfile.py
@@ -205,11 +205,6 @@ class Sysfile(Base):
         if self.vtype == self.SRBDS:
             self.sfile = self.SYS_SRBDS_FILE
 
-        if (os.path.exists(self.sfile)):
-            return self.sfile
-        else:
-            return None
-
         return self.sfile
 
     def is_option_valid(self, index, option, kernel_ver):

smtool/smtool_lib/variant.py
@@ -106,7 +106,7 @@ class Variant(Base):
         """
         self.mitigated_kernel = True
 
-        if self.sysfile.is_mitigated():
+        if (os.path.exists(self.sysfile.get_sysfile()) and self.sysfile.is_mitigated()):
             self.mitigated_sys = True
 
         vuln_string = self.boot.scan_cmdline()
@@ -588,11 +588,11 @@ class Variant(Base):
         if ((server.stype == self.XEN_PV) or (
                 server.stype == self.XEN_HVM) or
                 (server.stype == self.KVM_GUEST)):
-           if (not self.sysfile.is_mitigated()):
+           if (os.path.exists(self.sysfile.get_sysfile()) and not self.sysfile.is_mitigated()):
                return False
         else:
-            if ((cpu.is_vulnerable(self.vtype)) and (
-                not self.sysfile.is_mitigated())):
+            if ((cpu.is_vulnerable(self.vtype)) and (os.path.exists(self.sysfile.get_sysfile())
+                and not self.sysfile.is_mitigated())):
                 return False
         return True
 