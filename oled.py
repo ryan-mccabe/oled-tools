d998c1fb59e099372027b8ee22c0a3b67ec7341d
Manjunath Patil <manjunath.b.patil@oracle.com>
2022-12-20 10:40:48 -0800

    oled-tools: fix root user check for chroot env
    Present root user check makes oled-tools unusable in chroot envs.
    This patch uses os.geteuid() to make oled-tools useable in chroot envs.
    
    Orabug: 34909074
    Signed-off-by: Manjunath Patil <manjunath.b.patil@oracle.com>
    Reviewed-by: John Sobecki <john.sobecki@oracle.com>

--- oled.py
+++ oled.py
@@ -22,7 +22,6 @@
 
 import sys
 import os
-import getpass
 import platform
 
 # Oracle Linux Enhanced Diagnostic Tools
@@ -86,7 +85,7 @@ def is_uek4():
     return False
 
 def run_as_root():
-    if (getpass.getuser()) != "root":
+    if (os.geteuid() != 0):
         print("Run as root only")
         sys.exit(1)
 
