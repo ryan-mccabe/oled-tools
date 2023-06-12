5a45f3a03c56e1e7df8fedacd82f4a60b0ad480a
Manjunath Patil <manjunath.b.patil@oracle.com>
2023-06-12 22:26:35 +0000

    lkce: Fix linter errors - non space related
    This change fixes the other non space related linter errors.
    I used mypy, flake8 and pylint to detect errors.
    
    The following is the end result -
     # mypy lkce.py
     Success: no issues found in 1 source file
     # flake8 lkce.py
     # pylint lkce.py
     --------------------------------------------------------------------
     Your code has been rated at 10.00/10 (previous run: 10.00/10, +0.00)
    
     # mypy kdump_report.py
     Success: no issues found in 1 source file
     # flake8 kdump_report.py
     # pylint kdump_report.py
    
     --------------------------------------------------------------------
     Your code has been rated at 10.00/10 (previous run: 10.00/10, +0.00)
    
    Signed-off-by: Manjunath Patil <manjunath.b.patil@oracle.com>
    Reviewed-by: Laurence Rochfort <laurence.rochfort@oracle.com>
    Reviewed-by: John Haxby <john.haxby@oracle.com>
    Tested-by: Jeffery Yoder <jeffery.yoder@oracle.com>

--- tools/lkce/scripts/kdump_report.py
+++ tools/lkce/scripts/kdump_report.py
@@ -20,10 +20,10 @@
 # or visit www.oracle.com if you need additional information or have any
 # questions.
 
-#
-# program that runs inside kdump kernel
-# program to run crash utility inside the kdump kernel
-#
+"""
+Program that runs inside kdump kernel
+Program to run crash utility inside the kdump kernel
+"""
 import glob
 import time
 import os
@@ -32,46 +32,49 @@ import re
 import shutil
 import subprocess  # nosec
 import sys
-import signal
 
 
 class KdumpReport:
-    def __init__(self):
-        # global variables
-        self.VMLINUX = ""
-        self.VMCORE = "/proc/vmcore"
-        self.KDUMP_KERNELVER = platform.uname().release
+    """Class to include all kdump report related functionality"""
+    # pylint: disable=too-many-instance-attributes
+    def __init__(self) -> None:
+        """Constructor for KdumpReport class"""
+        self.vmlinux = ""
+        self.vmcore = "/proc/vmcore"
+        self.kdump_kernel_ver = platform.uname().release
 
-        self.KDUMP_REPORT_HOME = "/etc/oled/lkce"
-        self.KDUMP_REPORT_CONFIG_FILE = self.KDUMP_REPORT_HOME + "/lkce.conf"
-        self.KDUMP_REPORT_CRASH_CMDS_FILE = self.KDUMP_REPORT_HOME + "/crash_cmds_file"
-        self.KDUMP_REPORT_OUT = "/var/oled/lkce"
-        self.KDUMP_REPORT_OUT_FILE = self.KDUMP_REPORT_OUT + \
+        self.kdump_report_home = "/etc/oled/lkce"
+        self.kdump_report_config_file = self.kdump_report_home + "/lkce.conf"
+        self.kdump_report_crash_cmds_file = self.kdump_report_home + \
+            "/crash_cmds_file"
+        self.kdump_report_out = "/var/oled/lkce"
+        self.kdump_report_out_file = self.kdump_report_out + \
             "/crash_" + time.strftime("%Y%m%d-%H%M%S") + ".out"
-        self.TIMEDOUT_ACTION = "reboot -f"
+        self.timedout_action = "reboot -f"
 
         # default values
         self.kdump_report = "yes"
         self.vmlinux_path = "/usr/lib/debug/lib/modules/" + \
-            self.KDUMP_KERNELVER + "/vmlinux"
-        self.crash_cmds_file = self.KDUMP_REPORT_CRASH_CMDS_FILE
+            self.kdump_kernel_ver + "/vmlinux"
+        self.crash_cmds_file = self.kdump_report_crash_cmds_file
         self.max_out_files = "50"
 
-        self.read_config(self.KDUMP_REPORT_CONFIG_FILE)
+        self.read_config(self.kdump_report_config_file)
     # def __init__
 
-    def read_config(self, filename):
+    def read_config(self, filename: str) -> int:
+        """Read config file and update the class variables"""
         if not os.path.exists(filename):
-            self.exit()
+            sys.exit(1)
 
         try:
             file = open(filename, "r")
-        except:
-            print("kdump_report: Unable to operate on file: %s" % (filename))
-            return
+        except OSError:
+            print(f"kdump_report: Unable to operate on file: {filename}")
+            return 1
 
         for line in file.readlines():
-            if re.search("^#", line):# ignore lines starting with '#'
+            if re.search("^#", line):  # ignore lines starting with '#'
                 continue
 
             # trim space/tab/newline from the line
@@ -89,26 +92,35 @@ class KdumpReport:
 
             elif "max_out_files" in entry[0] and entry[1]:
                 self.max_out_files = entry[1]
+
+        return 0
     # def read_config
 
-    def get_vmlinux(self):
-        VMLINUX_1 = self.vmlinux_path
-        VMLINUX_2 = "/usr/lib/debug/lib/modules/" + self.KDUMP_KERNELVER + "/vmlinux"
+    def get_vmlinux(self) -> int:
+        """Check for vmlinux in config path and then in default location.
 
-        if os.path.exists(VMLINUX_1):
-            self.VMLINUX = VMLINUX_1
-        elif os.path.exists(VMLINUX_2):
-            self.VMLINUX = VMLINUX_2
+        Report error if not found
+        """
+        vmlinux_1 = self.vmlinux_path
+        vmlinux_2 = "/usr/lib/debug/lib/modules/" + \
+                    self.kdump_kernel_ver + "/vmlinux"
+
+        if os.path.exists(vmlinux_1) and os.path.isfile(vmlinux_1):
+            self.vmlinux = vmlinux_1
+        elif os.path.exists(vmlinux_2) and os.path.isfile(vmlinux_2):
+            self.vmlinux = vmlinux_2
         else:
-            print("kdump_report: vmlinux not found in the following locations.")
-            print("kdump_report: %s" % VMLINUX_1)
-            print("kdump_report: %s" % VMLINUX_2)
-            self.exit()
+            print("kdump_report: vmlinux not found in following locations.")
+            print("kdump_report: %s" % vmlinux_1)
+            print("kdump_report: %s" % vmlinux_2)
+            sys.exit(1)
 
-        print("kdump_report: vmlinux found at %s" % self.VMLINUX)
+        print("kdump_report: vmlinux found at %s" % self.vmlinux)
+        return 0
     # def get_vmlinux
 
-    def run_crash(self):
+    def run_crash(self) -> int:
+        """Run crash utility against vmcore"""
         crash_path = shutil.which("crash")
 
         if not crash_path:
@@ -116,26 +128,29 @@ class KdumpReport:
             return 1
 
         self.get_vmlinux()
-        if not os.path.exists(self.crash_cmds_file):
+        if not (os.path.exists(self.crash_cmds_file) and
+                os.path.isfile(self.crash_cmds_file)):
             print("kdump_report: %s not found" % self.crash_cmds_file)
             return 1
 
-        os.makedirs(self.KDUMP_REPORT_OUT, exist_ok=True)
+        os.makedirs(self.kdump_report_out, exist_ok=True)
 
-        args = (crash_path, self.VMLINUX, self.VMCORE, "-i",
+        args = (crash_path, self.vmlinux, self.vmcore, "-i",
                 self.crash_cmds_file)
         print(f"kdump_report: Executing '{' '.join(args)}'; output file "
-              f"'{self.KDUMP_REPORT_OUT_FILE}'")
+              f"'{self.kdump_report_out_file}'")
 
-        with open(self.KDUMP_REPORT_OUT_FILE, "w") as output_fd:
-            return subprocess.run(
-                args, close_fds=True, stdout=output_fd, stderr=output_fd,
-                stdin=subprocess.DEVNULL, shell=False)  # nosec
+        with open(self.kdump_report_out_file, "w") as output_fd:
+            subprocess.run(args, close_fds=True, stdout=output_fd,
+                           stderr=output_fd, stdin=subprocess.DEVNULL,
+                           shell=False, check=True)  # nosec
+        return 0
     # def run_crash
 
-    def clean_up(self):
+    def clean_up(self) -> None:
+        """Clean up old crash reports to save space"""
         max_files = int(self.max_out_files)
-        crash_files = glob.glob(f"{self.KDUMP_REPORT_OUT}/crash*out")
+        crash_files = glob.glob(f"{self.kdump_report_out}/crash*out")
 
         if len(crash_files) > max_files:
             print(f"kdump_report: found more than {max_files}[max_out_files] "
@@ -147,25 +162,21 @@ class KdumpReport:
                 except OSError:
                     pass  # ignore permissions and missing file errors
     # def clean_up
-
-    def exit(self):
-        sys.exit(0)
-    # def exit
-
 # class KDUMP_REPORT
 
 
-def main():
+def main() -> int:
+    """Main routine"""
     kdump_report = KdumpReport()
 
     if kdump_report.kdump_report != "yes":
         print("kdump_report: kdump_report is disabled to run")
-        kdump_report.exit()
+        sys.exit(1)
     else:
         print("kdump_report: kdump_report is enabled to run")
         kdump_report.run_crash()
         kdump_report.clean_up()
-        kdump_report.exit()
+        sys.exit(0)
 # def main
 
 
