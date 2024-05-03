5f47144d38aa4181f7702a4fba2e3429554a0439
Aruna Ramakrishna <aruna.ramakrishna@oracle.com>
2024-05-03 20:13:00 +0000

    memstate: Remove the '-a/--all' option
    The output from 'oled memstate -a' is now almost the same as that of
    'oled memstate' - so the option is redundant. Remove support for '-a'.
    
    Signed-off-by: Aruna Ramakrishna <aruna.ramakrishna@oracle.com>
    Reviewed-by: Jianfeng Wang <jianfeng.w.wang@oracle.com>
    Reviewed-by: Laurence Rochfort <laurence.rochfort@oracle.com>

--- tools/memstate/memstate.py
+++ tools/memstate/memstate.py
@@ -67,17 +67,7 @@ class Memstate(Base):
             print("")
             self.print_header = False
 
-    def memstate_opt_none(self):
-        """
-        Display the memory usage summary, and run a quick health check.
-        """
-        self.meminfo.display_usage_summary()
-        self.numa.memstate_check_numa()
-        self.slabinfo.memstate_check_slab()
-        self.rss.memstate_check_rss()
-        self.check_health()
-
-    def memstate_opt_all(self, verbose):
+    def memstate_opt_none(self, verbose):
         """
         Display memory usage summary, along with a list of user processes
         consuming most memory and swap, and run a quick health check.
@@ -288,8 +278,6 @@ def validate_args(args):
     """Validate user-provided arguments.
 
     Checks if there are any invalid combinations of arguments:
-      - "all" cannot be mixed with other options (except "verbose" and
-        "frequency")
       - "verbose" can be combined with any other option
       - "frequency" cannot be used if an input file is provided (for "slab" or
         "numa")
@@ -301,14 +289,6 @@ def validate_args(args):
             print(
                 "Option -n/--numa does not support -f/--frequency; "
                 "see usage for more details.")
-            return 1
-    if args.all:
-        # pylint: disable=too-many-boolean-expressions
-        if (args.pss
-                or (args.numa is not None and args.numa != 'nofile')):
-            print(
-                "Option -a/--all can only be combined with -v/--verbose and "
-                "-f/--frequency; see usage for more details.")
             return 1
     return 0
 
@@ -331,8 +311,6 @@ def main():
     parser.add_argument(
         "-n", "--numa", metavar="FILE", nargs="?", const="nofile",
         help="analyze/display NUMA stats")
-    parser.add_argument(
-        "-a", "--all", help="display all data", action="store_true")
     parser.add_argument(
         "-v", "--verbose", action="store_true",
         help="verbose data capture; combine with other options")
@@ -399,9 +377,6 @@ def main():
     memstate.memstate_header()
 
     while True:
-        if args.all:
-            args_passed = True
-            memstate.memstate_opt_all(opt_verbose)
         if args.pss:
             args_passed = True
             memstate.memstate_opt_pss(args.pss, opt_verbose)
@@ -412,13 +387,7 @@ def main():
             args_passed = True
             memstate.memstate_opt_numa(opt_verbose)
         if not args_passed:
-            if opt_verbose:
-                exit_with_msg(
-                    "Option -v/--verbose should be used in combination with "
-                    "other options; see -h/--help or the man page for more "
-                    "details.")
-                return
-            memstate.memstate_opt_none()
+            memstate.memstate_opt_none(opt_verbose)
 
         if interval == 0:
             break
